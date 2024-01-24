aa2854044168f0fa6b7b5240b457d0ffc8f28bda
Alius Petraška <alius.petraska@ibm.com>
2024-01-24 18:28:21 +0200

    intial commit

---
+++ nvidia.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+echo "[1] Verify You Have a CUDA-Capable GPU"
+
+lspci | grep -i nvidia
+
+echo "[2] Verify You Have a Supported Version of Linux"
+
+uname -m && cat /etc/*release
+
+echo "[3] Verify the System Has gcc Installed"
+
+gcc --version
+
+echo "[4] Verify the System has the Correct Kernel Headers and Development Packages Installed"
+
+uname -r
+
+echo "[5] Install NVIDIA GPU drivers"
+
+sudo dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel9/x86_64/cuda-rhel9.repo
+
+sudo dnf install kernel-devel-$(uname -r) kernel-headers-$(uname -r)
+
+sudo dnf install nvidia-driver nvidia-settings
+
+sudo dnf install cuda-driver
+
+sudo dnf install libcudnn8
+
+echo "[6] CLEANUP & RESTART"
+
+sudo dnf clean all
+
+sudo reboot
---
+++ prepare.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+echo "[1] UPDATING"
+
+sudo dnf update -y
+sudo dnf upgrade --refresh
+
+echo "[2] ADDING/ENABLING ADDITIONAL REPOS"
+
+sudo dnf config-manager --set-enabled crb
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo dnf install epel-release
+
+# https://computingforgeeks.com/install-use-ffmpeg-on-rocky-alma-9/
+sudo dnf install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm -y
+sudo dnf install --nogpgcheck https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm -y
+
+echo "[3] REPO LIST"
+
+sudo dnf repolist
+
+echo "[4] FFMPEG PREREQUISITES"
+
+sudo dnf install gcc clang cmake procps nano python3-pip git git-lfs ffmpeg ffmpeg-devel
+
+echo "[5] REMOVING PODMAN"
+
+sudo dnf remove podman buildah podman-compose
+
+echo "[6] INSTALLING DOCKER"
+
+# https://docs.rockylinux.org/gemstones/docker/
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+echo "[7] DEV PREREQUISITES"
+
+pip3 install pip -U
+git lfs install
+
+echo "[8] CLEANUP & RESTART"
+
+sudo dnf clean all
+
+sudo reboot
